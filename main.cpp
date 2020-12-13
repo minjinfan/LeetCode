39bd1175a0b6899a9dfe28170655649b9a17658d
minjinfan <minjinfan@gmail.com>
2020-12-13 11:02:41 +0800

    This is a combination of 2 commits. two --> one

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main(int argc, char* argv)
 {
-    cout <<  "Hello World!" << endl;
+    cout <<  "Hello World!" << endl;\
+    cout << "test1" << endl;
+    cout << "test2" << endl;
     return 0;
 }
