a3569be9b0bdcb2c2bb51b6f600c44fa568101fc
minjinfan <minjinfan@gmail.com>
2021-08-03 21:08:45 +0800

    rm gaussIntegral

--- leetCode/Common.hpp
+++ leetCode/Common.hpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
 typedef double real;
@@ -88,4 +90,19 @@ inline void faxpBy(vector<double> &Y, real& a, vector<double> &X, vector<double>
 	for(int i = 0; i < 3; ++i){
 		Y[i] = a * X[i] + B[i];
 	}
+}
+
+
+inline vec3 SubEq(const vec3 &X, const vec3 &Y)
+{
+	vec3 res(3,0);
+	for(int i = 0; i < 3; ++i){
+		res[i] = X[i] - Y[i];
+	}
+	return res;
+}
+
+inline double norm(const vec3 &X)
+{
+    return sqrt(pow(X[0],2) + pow(X[1],2) + pow(X[2],2));
 }
